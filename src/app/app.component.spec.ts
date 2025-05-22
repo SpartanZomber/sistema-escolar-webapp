d28958e3920d056b2d94adee44c454c864ec3237
SpartanZomber <jaredtoxic@gmail.com>
2025-05-22 05:20:47 -0600

    cambios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-escolar-webapp'`, () => {
+  it(`should have as title 'sistema-escolar-webapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-escolar-webapp');
+    expect(app.title).toEqual('sistema-escolar-webapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-escolar-webapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sistema-escolar-webapp app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crud-escolar-webapp';
+  title = 'sistema-escolar-webapp';
 }
